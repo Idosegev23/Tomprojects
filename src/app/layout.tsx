9a01d24125a177a3dcf1c5f26ad556fdb38794fe
idosegev23 <ido.segev23@gmail.com>
2025-03-31 08:58:20 +0300

    תיאור השינויים שביצעת

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Providers } from '@/components/layout/Providers'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
